d7fdebcab8acd4a022cfcb0fdd3e02aba70bb4fe
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-12-29 03:26:23 -0300

    refactor: Remove reading time
    Not useful or accurate, let's remove it

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,20 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders"
+import { defineCollection, z } from "astro:content"
 
 export const collections = {
-	blog: defineCollection({
-		// Load Markdown files in the src/content/blog directory.
-		loader: glob({ base: './src/content/blog', pattern: ['**/*.md', '**/*.mdx'] }),
-		schema: z.object({
-			title: z.string(),
-			publishDate: z.coerce.date(),
-			description: z.string(),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			tags: z.array(z.string()),
-			reading_time: z.string(),
-		}),
-	}),
-};
+  blog: defineCollection({
+    // Load Markdown files in the src/content/blog directory.
+    loader: glob({
+      base: "./src/content/blog",
+      pattern: ["**/*.md", "**/*.mdx"],
+    }),
+    schema: z.object({
+      title: z.string(),
+      publishDate: z.coerce.date(),
+      description: z.string(),
+      img: z.string(),
+      img_alt: z.string().optional(),
+      tags: z.array(z.string()),
+    }),
+  }),
+}
